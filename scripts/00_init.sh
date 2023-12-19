b297ddbbcaba793ffea1ca806f1b6a1ea52b1ec9
iqbal fasya <79214502+scoout@users.noreply.github.com>
2023-12-19 17:23:29 +0700

    Update 00_init.sh

--- scripts/00_init.sh
+++ scripts/00_init.sh
@@ -12,6 +12,7 @@ __get_other-repos() {
     git clone -b master --depth 1 --single-branch https://github.com/immortalwrt/immortalwrt immortalwrt
     git clone -b master --depth 1 --single-branch https://github.com/immortalwrt/packages immortalwrt-packages
     git clone -b master --depth 1 --single-branch https://github.com/immortalwrt/luci immortalwrt-luci
+    git clone --depth=1 https://github.com/helmiau/badvpn_corkscrew badvpn_corkscrew
 }
 
 __init_build_env() {
