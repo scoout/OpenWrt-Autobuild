17bb830a6f89135c1e7cb7100b47bdcd47db6cba
iqbal fasya <79214502+scoout@users.noreply.github.com>
2023-12-19 17:25:16 +0700

    Update x86-64_customization.sh

--- scripts/x86-64_customization.sh
+++ scripts/x86-64_customization.sh
@@ -6,6 +6,11 @@ set -ex
 echo '
 CONFIG_CRYPTO_GHASH=y
 CONFIG_CRYPTO_POLY1305_X86_64=y
+CONFIG_USB=y
+CONFIG_USB_ACM=y
+CONFIG_IOSM=y
+CONFIG_MPTCP=y
+CONFIG_MPTCP_IPV6=y
 ' >> ./target/linux/x86/64/config-6.1
 
 source ./01_customize_packages.sh
